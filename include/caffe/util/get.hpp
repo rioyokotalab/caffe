444a3ab357331e9d8fa59fe00cd463ee86984f39
Sergei Nikolaev <snikolaev@nvidia.com>
2015-07-30 15:26:50 -0700

    code cleanup

--- include/caffe/util/get.hpp
+++ include/caffe/util/get.hpp
@@ -4,148 +4,57 @@
 #include <cfloat>
 #include "caffe/util/fp16_conversion.hpp"
 
-template <typename T>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-T Get(unsigned int x);
-
-template <>
 #ifdef CPU_ONLY
-inline
+  #define CAFFE_UTIL_GET_HD
+  #define CAFFE_UTIL_GET_IHD inline
 #else
-__inline__ __host__ __device__
+  #define CAFFE_UTIL_GET_HD __host__ __device__
+  #define CAFFE_UTIL_GET_IHD __inline__ __host__ __device__
 #endif
-unsigned int Get(unsigned int x) { return x; }
 
-template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-int Get(unsigned int x) { return (int)x; }
-
-template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-float Get(unsigned int x) { return (float)x; }
-
-template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-double Get(unsigned int x) { return (float)x; }
 
 template <typename T>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-T Get(int x);
-
+CAFFE_UTIL_GET_HD T Get(unsigned int x);
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-int Get(int x) { return x; }
-
+CAFFE_UTIL_GET_IHD unsigned int Get(unsigned int x) { return x; }
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-float Get(int x) { return (float)x; }
-
+CAFFE_UTIL_GET_IHD int Get(unsigned int x) { return (int)x; }
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-double Get(int x) { return (float)x; }
+CAFFE_UTIL_GET_IHD float Get(unsigned int x) { return (float)x; }
+template <>
+CAFFE_UTIL_GET_IHD double Get(unsigned int x) { return (float)x; }
 
 template <typename T>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-T Get(float x);
-
+CAFFE_UTIL_GET_HD T Get(int x);
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-unsigned int Get(float x) { return (unsigned int)x; }
-
+CAFFE_UTIL_GET_IHD int Get(int x) { return x; }
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-int Get(float x) { return (int)x; }
-
+CAFFE_UTIL_GET_IHD float Get(int x) { return (float)x; }
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-float Get(float x) { return x; }
-
-template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-double Get(float x) { return (double)x; }
+CAFFE_UTIL_GET_IHD double Get(int x) { return (float)x; }
 
 template <typename T>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-T Get(double x);
-
+CAFFE_UTIL_GET_HD T Get(float x);
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-int Get(double x) { return (int)x; }
-
+CAFFE_UTIL_GET_IHD unsigned int Get(float x) { return (unsigned int)x; }
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-float Get(double x) { return (float)x; }
-
+CAFFE_UTIL_GET_IHD int Get(float x) { return (int)x; }
 template <>
-#ifdef CPU_ONLY
-inline
-#else
-__inline__ __host__ __device__
-#endif
-double Get(double x) { return x; }
+CAFFE_UTIL_GET_IHD float Get(float x) { return x; }
+template <>
+CAFFE_UTIL_GET_IHD double Get(float x) { return (double)x; }
 
+template <typename T>
+CAFFE_UTIL_GET_HD T Get(double x);
+template <>
+CAFFE_UTIL_GET_IHD int Get(double x) { return (int)x; }
+template <>
+CAFFE_UTIL_GET_IHD float Get(double x) { return (float)x; }
+template <>
+CAFFE_UTIL_GET_IHD double Get(double x) { return x; }
 
 template <typename L, typename R>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-L& Incr(L& l, const R& r) {
+CAFFE_UTIL_GET_HD L& Incr(L& l, const R& r) {
     double ld = Get<double>(l);
     ld += Get<double>(r);
     l = Get<L>(ld);
@@ -153,10 +62,7 @@ L& Incr(L& l, const R& r) {
 }
 
 template <typename L, typename R>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-L& Decr(L& l, const R& r) {
+CAFFE_UTIL_GET_HD L& Decr(L& l, const R& r) {
     double ld = Get<double>(l);
     ld -= Get<double>(r);
     l = Get<L>(ld);
@@ -164,10 +70,7 @@ L& Decr(L& l, const R& r) {
 }
 
 template <typename L, typename R>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-L& Mult(L& l, const R& r) {
+CAFFE_UTIL_GET_HD L& Mult(L& l, const R& r) {
     double ld = Get<double>(l);
     ld *= Get<double>(r);
     l = Get<L>(ld);
@@ -175,10 +78,7 @@ L& Mult(L& l, const R& r) {
 }
 
 template <typename L, typename R>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-L& Div(L& l, const R& r) {
+CAFFE_UTIL_GET_HD L& Div(L& l, const R& r) {
     double ld = Get<double>(l);
     ld /= Get<double>(r);
     l = Get<L>(ld);
@@ -186,25 +86,20 @@ L& Div(L& l, const R& r) {
 }
 
 template <typename T>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-float tol(float t) {
+CAFFE_UTIL_GET_HD float tol(float t) {
     return t;
 }
 
 template <typename T>
-#ifndef CPU_ONLY
-__host__ __device__
-#endif
-float maxDtype() {
+CAFFE_UTIL_GET_HD float maxDtype() {
     return FLT_MAX;
 }
 
 
 #ifndef CPU_ONLY
 
-template <> __inline__ __host__ __device__ half Get(unsigned int x) {
+template <>
+CAFFE_UTIL_GET_IHD half Get(unsigned int x) {
 #ifdef __CUDA_ARCH__
   half h;
   h.x = __float2half_rn(float(x));
@@ -213,8 +108,8 @@ template <> __inline__ __host__ __device__ half Get(unsigned int x) {
   return cpu_float2half_rn(float(x));
 #endif
 }
-
-template <> __inline__ __host__ __device__ half Get(int x) {
+template <>
+CAFFE_UTIL_GET_IHD half Get(int x) {
 #ifdef __CUDA_ARCH__
   half h;
   h.x = __float2half_rn(float(x));
@@ -223,8 +118,8 @@ template <> __inline__ __host__ __device__ half Get(int x) {
   return cpu_float2half_rn(float(x));
 #endif
 }
-
-template <> __inline__ __host__ __device__ half Get(float x) {
+template <>
+CAFFE_UTIL_GET_IHD half Get(float x) {
 #ifdef __CUDA_ARCH__
   half h;
   h.x = __float2half_rn(x);
@@ -233,8 +128,8 @@ template <> __inline__ __host__ __device__ half Get(float x) {
   return cpu_float2half_rn(x);
 #endif
 }
-
-template <> __inline__ __host__ __device__ half Get(double x) {
+template <>
+CAFFE_UTIL_GET_IHD half Get(double x) {
 #ifdef __CUDA_ARCH__
   half h;
   h.x = __float2half_rn(float(x));
@@ -244,41 +139,47 @@ template <> __inline__ __host__ __device__ half Get(double x) {
 #endif
 }
 
-template <typename T> __host__ __device__ T Get(const half& x);
-
-template <> __inline__ __host__ __device__ int Get(const half& x) { 
+template <typename T>
+CAFFE_UTIL_GET_HD T Get(const half& x);
+template <>
+CAFFE_UTIL_GET_IHD int Get(const half& x) {
 #ifdef __CUDA_ARCH__
   return int(__half2float(x));
 #else
   return int(cpu_half2float(x));
 #endif
 }
-template <> __inline__ __host__ __device__ float Get(const half& x) {
+template <>
+CAFFE_UTIL_GET_IHD float Get(const half& x) {
 #ifdef __CUDA_ARCH__
   return __half2float(x);
 #else
   return cpu_half2float(x);
 #endif
 }
-template <> __inline__ __host__ __device__ double Get(const half& x) {
+template <>
+CAFFE_UTIL_GET_IHD double Get(const half& x) {
 #ifdef __CUDA_ARCH__
   return double(__half2float(x));
 #else
   return double(cpu_half2float(x));
 #endif
 }
-template <> __inline__ __host__ __device__ half Get(const half& x) { return x; }
+template <>
+CAFFE_UTIL_GET_IHD half Get(const half& x) { return x; }
 
-__inline__ __host__ __device__
+CAFFE_UTIL_GET_IHD
 bool operator < (const half& l, const half& r) {
     return Get<float>(l) < Get<float>(r);
 }
 
-template <> __inline__ __host__ __device__ float tol<half>(float t) {
+template <>
+CAFFE_UTIL_GET_IHD float tol<half>(float t) {
     return t < 1.e-4 ? 2.5e-2 : t * 2.5e2;
 }
 
-template <> __inline__ __host__ __device__ float maxDtype<half>() {
+template <>
+CAFFE_UTIL_GET_IHD float maxDtype<half>() {
     return 65504.0F;
 }
 
