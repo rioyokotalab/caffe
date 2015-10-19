8040296fc8a6aac66497b2ea478492e7f93d7f23
Boris Fomitchev <bfomitchev@nvidia.com>
2015-10-18 18:51:52 -0700

    Refactoring for fp16

--- src/caffe/layer.cpp
+++ src/caffe/layer.cpp
@@ -3,33 +3,22 @@
 
 namespace caffe {
 
-template <typename Dtype, typename Mtype>
-void Layer<Dtype,Mtype>::InitMutex() {
+void LayerBase::InitMutex() {
   forward_mutex_.reset(new boost::mutex());
 }
 
-template <typename Dtype, typename Mtype>
-void Layer<Dtype,Mtype>::Lock() {
+
+void LayerBase::Lock() {
   if (IsShared()) {
     forward_mutex_->lock();
   }
 }
 
-template <typename Dtype, typename Mtype>
-void Layer<Dtype,Mtype>::Unlock() {
+void LayerBase::Unlock() {
   if (IsShared()) {
     forward_mutex_->unlock();
   }
 }
-
-template
-void Layer<half,half>::InitMutex();
-
-template
-void Layer<half,half>::Lock();
-
-template
-void Layer<half,half>::Unlock();
 
 INSTANTIATE_CLASS(Layer);
 
