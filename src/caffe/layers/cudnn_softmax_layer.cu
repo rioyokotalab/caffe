dcd07545bb8a8e69b364d6886abc027ea55ab1f2
thatguymike <mhouston@nvidia.com>
2015-10-24 00:32:22 +0000

    Switch to default softmax as it's currently faster for small batch

--- src/caffe/layers/cudnn_softmax_layer.cu
+++ src/caffe/layers/cudnn_softmax_layer.cu
@@ -14,33 +14,13 @@ namespace caffe {
 template <typename Dtype, typename Mtype>
 void CuDNNSoftmaxLayer<Dtype,Mtype>::Forward_gpu(const vector<Blob<Dtype,Mtype>*>& bottom,
     const vector<Blob<Dtype,Mtype>*>& top) {
-  const Dtype* bottom_data = bottom[0]->gpu_data();
-  Dtype* top_data = top[0]->mutable_gpu_data();
-  CUDNN_CHECK(cudnnSoftmaxForward(Caffe::cudnn_handle(), CUDNN_SOFTMAX_ACCURATE,
-        CUDNN_SOFTMAX_MODE_CHANNEL,
-        cudnn::dataType<Dtype>::one,
-        bottom_desc_, bottom_data,
-        cudnn::dataType<Dtype>::zero,
-        top_desc_, top_data));
+    SoftmaxLayer<Dtype,Mtype>::Forward_gpu(bottom, top);
 }
 
 template <typename Dtype, typename Mtype>
 void CuDNNSoftmaxLayer<Dtype,Mtype>::Backward_gpu(const vector<Blob<Dtype,Mtype>*>& top,
     const vector<bool>& propagate_down, const vector<Blob<Dtype,Mtype>*>& bottom) {
-  if (propagate_down[0]) {
-    const Dtype* top_data = top[0]->gpu_data();
-    const Dtype* top_diff = top[0]->gpu_diff();
-    const Dtype* bottom_data = bottom[0]->gpu_data();
-    Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
-
-    CUDNN_CHECK(cudnnSoftmaxBackward(
-          Caffe::cudnn_handle(), CUDNN_SOFTMAX_ACCURATE,
-          CUDNN_SOFTMAX_MODE_CHANNEL,
-          cudnn::dataType<Dtype>::one,
-          top_desc_, top_data, top_desc_, top_diff,
-          cudnn::dataType<Dtype>::zero,
-          bottom_desc_, bottom_diff));
-  }
+    SoftmaxLayer<Dtype,Mtype>::Backward_gpu(top, propagate_down, bottom);
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(CuDNNSoftmaxLayer);
