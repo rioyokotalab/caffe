ee979bdff00379aba0d453d7aada804826c20246
thatguymike <mhouston@nvidia.com>
2015-11-26 02:12:43 +0000

    Neglected to enable cub allocator by default

--- tools/caffe_fp16.cpp
+++ tools/caffe_fp16.cpp
@@ -323,7 +323,7 @@ int main(int argc, char** argv) {
 
   vector<int> gpus;
   get_gpus(&gpus);
-  caffe::gpu_memory::arena arena(gpus,caffe::gpu_memory::CnMemPool /* , true */);
+  caffe::gpu_memory::arena arena(gpus,caffe::gpu_memory::CubPool /* , true */);
 
   if (argc == 2) {
 #ifdef WITH_PYTHON_LAYER
